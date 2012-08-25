1c0d45ba9b089651c7694eb934a611ac824a3450
Bentsi <magidovich@gmail.com>
2012-08-25 21:53:33 +0300

    Gui barebone

--- lfm.py
+++ lfm.py
@@ -3,13 +3,16 @@ import datetime
 import urllib
 import re
 import os
+import wx
 
-# You have to have your own unique two values for API_KEY and API_SECRET
-# Obtain yours from http://www.last.fm/api/account for Last.fm
 API_KEY =    "e09f752b88d2e0d7d71b8f178d931970" 
 API_SECRET = "8562cb7ab597776e0f92cabf6fc19dda"
 Metro_URL = "http://ws.audioscrobbler.com/2.0/?method=geo.getmetros&api_key="+API_KEY
 Dates_URL = "http://ws.audioscrobbler.com/2.0/?method=geo.getmetroweeklychartlist&api_key="+API_KEY
+
+
+
+
 
 def duration2min(time):
     totalTime = int(time)/60
@@ -33,7 +36,7 @@ def retriveDates():
     return tmpList
 
 
-LIST_OF_Metroes=retriveListOfMetroes()
+
 Timer=0
 
 
@@ -57,6 +60,7 @@ def listoftime():
     return ListofDate
 
 def Download():
+    LIST_OF_Metroes=retriveListOfMetroes()
     ListofDate=listoftime()
     Timer=0
     for CDate in range(len(ListofDate)):
@@ -75,7 +79,71 @@ def Download():
             f.close()
         else:
             Timer+=214
-            print ListofDate[CDate][1]
+            print ListofDate[CDate][1]     
+#Download()
+#GUI part
+class MainWindow(wx.Frame):
+    def __init__(self, *args, **kw):
+        super(MainWindow, self).__init__(*args, **kw) 
+        self.InitUI()
+    def InitUI(self):
+        pnl = wx.Panel(self)
+        #creating sizers
+        verticalBox = wx.BoxSizer(wx.VERTICAL)
+        horizontalBox1 = wx.BoxSizer(wx.HORIZONTAL)
+        horizontalBox2 = wx.BoxSizer(wx.HORIZONTAL)
+        horizontalBox3 = wx.BoxSizer(wx.HORIZONTAL)
+        horizontalBox4 = wx.BoxSizer(wx.HORIZONTAL)
+        horizontalBox5 = wx.BoxSizer(wx.HORIZONTAL)
+        #creating widgets
+        #static text
+        self.text1 = wx.StaticText(pnl,label="\nPlease select weeks range in format <yyyyww>")
+        self.text2 = wx.StaticText(pnl,label="Beginning from:\t")
+        self.text3 = wx.StaticText(pnl,label="\tEnding on:")
+
+        #Text Controls
+        self.fromTextBox = wx.TextCtrl(pnl,style = wx.TE_CENTRE)
+        self.endTextBox = wx.TextCtrl(pnl, style = wx.TE_CENTRE)
+        #applying to box sizers
+        horizontalBox1.Add(self.text1,proportion=1)
+        horizontalBox2.Add(self.text2,proportion=1,flag = wx.ALIGN_CENTRE)
+        horizontalBox2.Add(self.fromTextBox,proportion=1)
+        horizontalBox2.Add(self.text3,proportion=1,flag = wx.ALIGN_CENTRE)
+        horizontalBox2.Add(self.endTextBox, proportion=1)
+        verticalBox.Add(horizontalBox1,flag = wx.ALIGN_CENTRE)
+        verticalBox.Add((0,20))
+        verticalBox.Add(horizontalBox2,flag = wx.ALIGN_CENTRE)
+        #setting main panel sizer
+        pnl.SetSizer(verticalBox)
+        #window properties
+        self.SetSize((500,300))
+        self.SetTitle("Last.fm downloader")
+        self.Centre()
+        self.Show(True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
-           
-Download()
+
+app = wx.App()
+MainWindow(None)
+app.MainLoop()
